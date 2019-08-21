53e046c3cbb1fb056dd1e46656db691061398faf
Joe Cooper <josephcooper3@gmail.com>
2019-08-21 14:46:44 +0100

    add index and show view for students

--- students_controller.rb
+++ students_controller.rb
@@ -5,8 +5,15 @@ require_relative('models/house')
 also_reload('./models/*')
 
 # index
+get '/index' do
+  erb(:index)
+end
 
 # show
+get '/students' do
+  @students = Student.all()
+  erb(:show)
+end
 
 # new
 
